a553038ecdd3b0463ea31633f846196b95e2884f
NVSSC8 <sricharan.ram2000@@gmail.com>
2026-07-17 11:22:43 +0530

    added another dependency in pom-xml

--- backend/src/test/java/com/agritech/AgritechBackendApplicationTests.java
+++ backend/src/test/java/com/agritech/AgritechBackendApplicationTests.java
@@ -2,12 +2,14 @@ package com.agritech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;   // <-- add this import
 
 @SpringBootTest
+@ActiveProfiles("test")   // <-- add this line
 class AgritechBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
